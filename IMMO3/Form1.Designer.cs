7931e2eecb2895c9d073082e3ef2ce4ed833e94e
bleskaces <90737853+bleskaces@users.noreply.github.com>
2022-06-11 13:15:52 +0700

    Add files via upload

---
+++ IMMO3/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace IMMO3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.btn_Stop = new System.Windows.Forms.Button();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.btn_Create = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nud_Rules = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nud_Columns = new System.Windows.Forms.NumericUpDown();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_Rules)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_Columns)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.splitContainer1.Panel1.Controls.Add(this.btn_Stop);
+            this.splitContainer1.Panel1.Controls.Add(this.btn_Start);
+            this.splitContainer1.Panel1.Controls.Add(this.btn_Create);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.nud_Rules);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.nud_Columns);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView);
+            this.splitContainer1.Size = new System.Drawing.Size(1012, 441);
+            this.splitContainer1.SplitterDistance = 280;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // btn_Stop
+            // 
+            this.btn_Stop.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.btn_Stop.Enabled = false;
+            this.btn_Stop.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_Stop.ForeColor = System.Drawing.Color.Black;
+            this.btn_Stop.Location = new System.Drawing.Point(154, 342);
+            this.btn_Stop.Name = "btn_Stop";
+            this.btn_Stop.Size = new System.Drawing.Size(96, 44);
+            this.btn_Stop.TabIndex = 6;
+            this.btn_Stop.Text = "Stop";
+            this.btn_Stop.UseVisualStyleBackColor = false;
+            this.btn_Stop.Click += new System.EventHandler(this.btn_Stop_Click);
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_Start.Enabled = false;
+            this.btn_Start.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_Start.ForeColor = System.Drawing.Color.Black;
+            this.btn_Start.Location = new System.Drawing.Point(45, 342);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(77, 44);
+            this.btn_Start.TabIndex = 5;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = false;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            this.btn_Start.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btn_Start_MouseDown);
+            // 
+            // btn_Create
+            // 
+            this.btn_Create.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.btn_Create.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_Create.ForeColor = System.Drawing.Color.Black;
+            this.btn_Create.Location = new System.Drawing.Point(86, 49);
+            this.btn_Create.Name = "btn_Create";
+            this.btn_Create.Size = new System.Drawing.Size(119, 44);
+            this.btn_Create.TabIndex = 4;
+            this.btn_Create.Text = "Create";
+            this.btn_Create.UseVisualStyleBackColor = false;
+            this.btn_Create.Click += new System.EventHandler(this.btn_Create_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(182, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 28);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Rules";
+            // 
+            // nud_Rules
+            // 
+            this.nud_Rules.Location = new System.Drawing.Point(186, 141);
+            this.nud_Rules.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.nud_Rules.Name = "nud_Rules";
+            this.nud_Rules.Size = new System.Drawing.Size(64, 20);
+            this.nud_Rules.TabIndex = 2;
+            this.nud_Rules.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.nud_Rules.Value = new decimal(new int[] {
+            110,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(23, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Columns ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // nud_Columns
+            // 
+            this.nud_Columns.Location = new System.Drawing.Point(45, 141);
+            this.nud_Columns.Minimum = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.nud_Columns.Name = "nud_Columns";
+            this.nud_Columns.Size = new System.Drawing.Size(57, 20);
+            this.nud_Columns.TabIndex = 0;
+            this.nud_Columns.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.nud_Columns.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.AllowUserToResizeColumns = false;
+            this.dataGridView.AllowUserToResizeRows = false;
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.BackgroundColor = System.Drawing.Color.Silver;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.ColumnHeadersVisible = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView.GridColor = System.Drawing.Color.Black;
+            this.dataGridView.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView.MultiSelect = false;
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowHeadersVisible = false;
+            this.dataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView.Size = new System.Drawing.Size(724, 437);
+            this.dataGridView.TabIndex = 0;
+            this.dataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellContentClick);
+            this.dataGridView.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView_CellMouseClick);
+            this.dataGridView.SelectionChanged += new System.EventHandler(this.dataGridView_SelectionChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1012, 441);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.nud_Rules)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_Columns)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btn_Create;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nud_Rules;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nud_Columns;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.Button btn_Stop;
+    }
+}
+
---
+++ IMMO3/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace IMMO3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+               
+        string[] positions = new string[] { "111", "110", "101", "100", "011", "010", "001", "000" }; // поиск позиций по правилам
+        char[] cellRules; // число правил
+        bool start = true;        
+        int rowCounter = 0; // кол-во строк
+              
+        private char[] acceptRules(int rule)
+        {
+            char[] result;
+
+            string binaryCode = Convert.ToString(rule, 2);
+
+            int binaryLength = binaryCode.Length;
+            if (binaryLength != 8)
+            {
+                for (int i = 0; i < 8 - binaryLength; i++)
+                {
+                    binaryCode = "0" + binaryCode;
+                }
+            }
+
+            result = binaryCode.ToCharArray();
+
+            return result;
+        }
+
+        private char calculateLayerCellValue(char[] xyz)
+        {
+            char result;
+
+            string code = new string(xyz);
+
+            int index = Array.IndexOf(positions, code);
+
+            result = cellRules[index];
+
+            return result;
+        }
+
+        private void btn_Create_Click(object sender, EventArgs e)
+        {            
+            nud_Columns.Enabled = false; 
+            nud_Rules.Enabled = false;        
+            btn_Start.Enabled = true;         
+
+            dataGridView.Rows.Clear();       // Очищается вся таблица
+                     
+            for (int i = 0; i < dataGridView.Columns.Count; i++)
+            {
+                dataGridView.Columns.RemoveAt(0);
+            }
+                  
+            for (int i = 0; i < (int)nud_Columns.Value; i++) // добавление колонок
+            {
+                dataGridView.Columns.Add("", "");
+            }
+                       
+            dataGridView.Rows.Add(); // первая - базовая строка
+                   
+            int rule = (int)nud_Rules.Value;
+            cellRules = acceptRules(rule);
+        }
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {            
+            btn_Create.Enabled = false;  
+            btn_Stop.Enabled = true;    
+            btn_Start.Enabled = false;   
+
+            start = false;              
+
+            rowCounter = 0;
+            timer1.Start();             // Запуск программы
+        }
+
+        private void btn_Stop_Click(object sender, EventArgs e)
+        {            
+            timer1.Stop();                      
+            btn_Create.Enabled = true;           
+            btn_Stop.Enabled = false;            
+            start = true;                      
+            nud_Columns.Enabled = true;     
+            nud_Rules.Enabled = true;
+        }
+              
+        private void dataGridView_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (start)
+                switch (e.Button)
+                {
+                    case MouseButtons.Left:
+                        dataGridView[e.ColumnIndex, e.RowIndex].Style.BackColor = Color.DarkGreen;
+                        dataGridView.ClearSelection();
+                        break;
+                    case MouseButtons.Right:
+                        dataGridView[e.ColumnIndex, e.RowIndex].Style.BackColor = Color.White;
+                        dataGridView.ClearSelection();
+                        break;
+                }
+        }
+                
+        private void dataGridView_SelectionChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                ((DataGridView)sender).SelectedCells[0].Selected = false;
+            }
+            catch { }
+        }
+             
+        private void timer1_Tick(object sender, EventArgs e)
+        {           
+            char[] previousLayer = new char[dataGridView.Columns.Count]; // прошлое поколение
+                        
+            char[] currentLayer = new char[dataGridView.Columns.Count]; // следующее поколение 
+                        
+            char[] xyz = new char[3];
+
+            
+            for (int i = 0; i < previousLayer.Length; i++) // заполнение предыдущих значениями 
+            {
+                if (dataGridView[i, rowCounter].Style.BackColor == Color.DarkGreen) previousLayer[i] = '1';
+                else previousLayer[i] = '0';
+            }
+           
+            dataGridView.Rows.Add(); // новая строка
+            rowCounter++;
+                       
+            for (int i = 0; i < currentLayer.Length; i++) // вычисление значения нового поколения
+            {
+                xyz[0] = previousLayer[(i + previousLayer.Length - 1) % previousLayer.Length];
+                xyz[1] = previousLayer[i];
+                xyz[2] = previousLayer[(i + previousLayer.Length + 1) % previousLayer.Length];
+                currentLayer[i] = calculateLayerCellValue(xyz);
+            }
+                      
+            for (int i = 0; i < currentLayer.Length; i++)
+            {
+                if (currentLayer[i] == '0') dataGridView[i, rowCounter].Style.BackColor = Color.White;
+                else dataGridView[i, rowCounter].Style.BackColor = Color.DarkGreen;
+            }
+        }
+
+        private void dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Start_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
